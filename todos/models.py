3e22c822b3133517a65c743768bdfd43eab632dd
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-06-05 15:19:26 -0400

    set up app and registration

---
+++ todos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    birthdate = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
