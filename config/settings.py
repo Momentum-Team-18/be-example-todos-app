3df79b24950339fc57024338a4f8de15d22fecd5
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-06-06 14:35:28 -0400

    update settings with .env and postgres

--- config/settings.py
+++ config/settings.py
@@ -9,18 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# contents of my .env file
+# SECRET_KEY=django-insecure-7mti)gfdehm&0&byyt6cpwv#j+f9q2j)s(ruosz951+#k2&47=
+# DATABASE_URL=postgres://todos:@127.0.0.1:5432/todos
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7mti)gfdehm&0&byyt6cpwv#j+f9q2j)s(ruosz951+#k2&47='
+SECRET_KEY = env('SECRET_KEY')
+# go get from .env file
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,13 +86,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
+    # works with the DATABASE_URL value in .env
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
